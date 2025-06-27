d53e5e58ffbeec71a167c4296f5dbd519e15a5ed
Jhon Garcerant <jgarcerant@fgs.co>
2025-06-27 11:58:42 -0500

    Modifing createReport method to protected

--- 01-creacionales/02.2-factory-method.ts
+++ 01-creacionales/02.2-factory-method.ts
@@ -54,7 +54,7 @@ class AccountingReport implements Report {
 // 3. Clase Base ReportFactory con el Método Factory
 
 abstract class ReportFactory {
-  abstract createReport(): Report;
+  protected abstract createReport(): Report;
 
   generateReport(): void {
     const report = this.createReport();
