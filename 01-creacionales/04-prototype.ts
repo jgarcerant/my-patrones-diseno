354e7ae0befd15a1958b46919d9bd0f5c9479ef3
Jhon Garcerant <jgarcerant@fgs.co>
2025-08-15 15:21:51 -0500

    feat(creacionales): Implementa patrón Prototype
    Se añaden ejemplos y ejercicios del patrón de diseño Prototype.
    
    - Se implementa un caso de uso básico con documentos.
    
    - Se desarrolla un ejercicio práctico con Pokémons para ilustrar la clonación profunda y la independencia de los objetos clonados.
    
    - Se actualiza la configuración de VSCode para mejorar la organización visual del proyecto.

--- 01-creacionales/04-prototype.ts
+++ 01-creacionales/04-prototype.ts
@@ -3,9 +3,52 @@
 
  * Es un patrón de diseño creacional que nos permite copiar objetos existentes sin hacer
  * que el código dependa de sus clases.
- * 
- * * Es útil cuando queremos duplicar el contenido, 
+ *
+ * * Es útil cuando queremos duplicar el contenido,
  * * el título y el autor de un documento, por ejemplo o cualquier objeto complejo.
- * 
+ *
  * https://refactoring.guru/es/design-patterns/prototype
  */
+
+class Document {
+  public title: string;
+  public author: string;
+  private content: string;
+
+  constructor(title: string, author: string, content: string) {
+    this.title = title;
+    this.author = author;
+    this.content = content;
+  }
+
+  clone() {
+    return new Document(this.title, this.author, this.content);
+  }
+
+  displayInfo() {
+    console.log(`
+        Title: ${this.title}
+        Author: ${this.author}
+        Content: ${this.content}
+    `);
+  }
+}
+
+function main() {
+  const document1 = new Document(
+    "The Great Gatsby",
+    "F. Scott Fitzgerald",
+    "The story of a man who falls in love with a woman from a wealthy family..."
+  );
+
+  console.log({ document1 });
+  document1.displayInfo();
+
+  const document2 = document1.clone();
+  document2.title = "El Gran Gatsby";
+
+  console.log({ document2 });
+  document2.displayInfo();
+}
+
+main();
